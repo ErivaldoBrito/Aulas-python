4de2c50ee49c36cd6e30cc2f205d074e5b229566
ErivaldoBrito <erivaldo.brito@ba.estudante.senai.br>
2024-08-16 18:14:14 +0000

    Comandos Iniciais.

---
+++ 1. Inicio do Pyton - Primitivos/Tipagem dinamica.py
@@ -0,0 +1,12 @@
+import os
+
+os.system("cls || clear")
+
+# Tipagem dinamica.py
+nome = "Marta"
+idade = 20
+altura = 1.65
+
+print(f"Nome: {nome}")
+print(f"Idade: {idade}")
+print(f"Altura: {altura}")
